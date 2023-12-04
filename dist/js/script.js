2c8ad52a4a50069e6bcd4edd69d619262b89a01f
Ikurnia27 <152824332+Ikurnia27@users.noreply.github.com>
2023-12-04 07:52:57 +0700

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,8 @@
+const menuBar = document.querySelector(".menu-bar");
+const menuNav = document.querySelector(".menu-navigation");
+
+menuBar.addEventListener("click", function(){
+    menuNav.classList.toggle("menu-active");
+});
+
+
